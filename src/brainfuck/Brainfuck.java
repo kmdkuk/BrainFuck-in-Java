3dc7051cc7154431b999bba5096a4de07499f62e
kouki <kouworld0123@gmail.com>
2017-07-18 21:53:40 +0900

    ,[]以外のコマンドに対応

--- src/brainfuck/Brainfuck.java
+++ src/brainfuck/Brainfuck.java
@@ -2,5 +2,60 @@ package brainfuck;
 
 public class Brainfuck
 {
+	String sourceCode = "";
+	private int codePointer;
+	private final int memoryNumber = 20;
+	private int ptr;
+	private int memory[] = new int[memoryNumber];
 
+	Brainfuck(String sourceCode)
+	{
+		this.sourceCode = sourceCode;
+	}
+
+	public String run()
+	{
+		String result = "";
+		init();
+		while(sourceCode.length() > codePointer)
+		{
+			char command = sourceCode.charAt(codePointer);
+			switch(command)
+			{
+			case '>':
+				ptr++;
+				break;
+			case '<':
+				ptr--;
+				break;
+			case '+':
+				memory[ptr]++;
+				break;
+			case '-':
+				memory[ptr]--;
+				break;
+			case '.':
+				result += (char)memory[ptr];
+				break;
+			case ',':
+
+				break;
+			case '[':
+				break;
+			case ']':
+				break;
+			default:
+				break;
+			}
+			codePointer++;
+		}
+		return result;
+	}
+
+	private void init()
+	{
+		codePointer = 0;
+		ptr = 0;
+		for(int i=0;i<memoryNumber;i++)memory[i] = 0;
+	}
 }
